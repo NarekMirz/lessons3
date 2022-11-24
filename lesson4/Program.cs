86239842eb9e345cc024bfe6a536d7da3243ddef
Narek <mirzakhanyan99@list.ru>
2022-11-24 17:56:20 +0300

    Готовое

---
+++ lesson4/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+System.Console.WriteLine("Введите координату X1: ");
+double x1 = double.Parse(Console.ReadLine() ?? "");
+
+System.Console.WriteLine("Введите координату X2: ");
+double x2 = double.Parse(Console.ReadLine() ?? "");
+
+System.Console.WriteLine("Введите координату Y1: ");
+double y1 = double.Parse(Console.ReadLine() ?? "");
+
+System.Console.WriteLine("Введите координату Y2: ");
+double y2 = double.Parse(Console.ReadLine() ?? "");
+
+System.Console.WriteLine("Введите координату Z1: ");
+double z1 = double.Parse(Console.ReadLine() ?? "");
+
+System.Console.WriteLine("Введите координату Z2: ");
+double z2 = double.Parse(Console.ReadLine() ?? "");
+
+double result = distancePoint(x1,y1,x2,y2,z1,z2);
+System.Console.WriteLine(result);
+
+static double distancePoint(double x1,double y1,double x2,double y2,double z1,double z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1,2));
+}
+Console.ReadLine();
